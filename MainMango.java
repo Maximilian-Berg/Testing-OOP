e94106f09a7be3f13c7f19a2efde3df2acc2a45f
Max <35370679+Maximilian-Berg@users.noreply.github.com>
2018-01-14 01:14:41 +0800

    Fixed renaming error

--- MainMango.java
+++ MainMango.java
@@ -9,20 +9,22 @@ public class MainMango {
     System.out.println("Your dog is now called " + named);
 
     boolean renameConsent = false;
-    System.out.print("Would you like to rename your dog? Y/N: ");
-    String rename = inpt.next();
-    if (rename == "Y" || rename == "y") {
+    System.out.print("Would you like to rename your dog? true/false: ");
+    Boolean rename = inpt.nextBoolean();
+    /* if (rename == "Y" || rename == "y") {
       renameConsent = true;
     } else if (rename == "N" || rename == "n") {
     } else {
       System.out.println("I am sorry, but I do not understand that.");
     }
-
-    if (renameConsent == true) {
+*/
+    if (rename == true) {
       System.out.print("Please enter a new name for your dog: ");
       mine.rename(inpt.next());
       named = mine.getName();
-      System.out.println("Your dog is now called" + named);
+      System.out.println("Your dog is now called " + named);
+    } else {
+      System.out.println("That's fine too.");
     }
   }
 }
