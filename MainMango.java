c2c990b93733d4d95c708e5326d512164b052571
Max <35370679+Maximilian-Berg@users.noreply.github.com>
2018-01-14 01:19:47 +0800

    Cleaned up code

--- MainMango.java
+++ MainMango.java
@@ -7,17 +7,9 @@ public class MainMango {
     Dog mine = new Dog(inpt.next());
     String named = mine.getName();
     System.out.println("Your dog is now called " + named);
-
     boolean renameConsent = false;
     System.out.print("Would you like to rename your dog? true/false: ");
     Boolean rename = inpt.nextBoolean();
-    /* if (rename == "Y" || rename == "y") {
-      renameConsent = true;
-    } else if (rename == "N" || rename == "n") {
-    } else {
-      System.out.println("I am sorry, but I do not understand that.");
-    }
-*/
     if (rename == true) {
       System.out.print("Please enter a new name for your dog: ");
       mine.rename(inpt.next());
