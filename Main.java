17819a40ad9bec4838d691c97258500bf54f9e38
Max <35370679+Maximilian-Berg@users.noreply.github.com>
2018-01-13 18:58:52 +0800

    Modified code, so name of dog is chosen and rename is optional + chosen

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class Main {
   public static void main(String[] Args) {
-      Dog mine = new Dog("Sparkie");
-      String named = mine.getName();
-      System.out.println(named);
-      mine.rename("Spiky");
+    Scanner inpt = new Scanner(System.in);
+    System.out.print("Please enter a Name for your dog: ");
+    Dog mine = new Dog(inpt.next());
+    String named = mine.getName();
+    System.out.println("Your dog is now called " + named);
+
+    boolean renameConsent;
+    System.out.print("Would you like to rename your dog? Y/N: ");
+    char rename = inpt.next();
+    if (rename == 'Y' || rename == 'y') {
+      renameConsent = true;
+    } else if (rename == 'N' || rename == 'n') {
+      renameConsent = false;
+    } else {
+      System.out.println("I am sorry, but I do not understand that.")
+    }
+
+    if (renameConsent == true) {
+      System.out.print("Please enter a new name for your dog: ");
+      mine.rename(inpt.next());
       named = mine.getName();
-      System.out.println(named);
+      System.out.println("Your dog is now called" + named);
+    }
   }
 }
